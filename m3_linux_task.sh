3581fc252cc3add5b0542a731326807cea1df543
kotovas <IQkotov@ya.ru>
2016-11-18 23:12:54 +0400

    Update m3_linux_task.sh

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,1 +1,1 @@
-wget -qO - yandex.ru| grep -oE 'link_black_yes" aria-label="[^"]+"'> bla; cat bla| sed -e 's/link_black_yes" aria-label=//g'> weather; cat weather;
+wget -qO - http://www.yandex.ru| grep -oE 'link_black_yes" aria-label="[^"]+"'> bla; cat bla| sed -e 's/link_black_yes" aria-label=//g'> last_news; cat last_news;
