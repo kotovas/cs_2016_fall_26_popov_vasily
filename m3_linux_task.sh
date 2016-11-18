90e3a60a4b0453b8515902775602f6fc7d1e6d20
kotovas <IQkotov@ya.ru>
2016-11-18 23:09:16 +0400

    Update m3_linux_task.sh

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,1 +1,1 @@
-wget https://www.yandex.ru/;cat index.html|grep -o -E 'yes" aria-label="[^"]+"' >text1;cat text1|sed -e 's/yes" aria-label=//g' >text2;cat text2|sed -e 's/"Котировки"/Погода/g' >text1;rm index.html;rm text2;
+wget -qO - yandex.ru| grep -oE 'link_black_yes" aria-label="[^"]+"'> bla; cat bla| sed -e 's/link_black_yes" aria-label=//g'> weather; cat weather;
