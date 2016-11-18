a95ca374c6881014702e3d1949d0a043ef3f02f1
kotovas <IQkotov@ya.ru>
2016-11-18 22:01:07 +0300

    Create m3_linux_task.sh

---
+++ m3_linux_task.sh
@@ -0,0 +1,1 @@
+wget https://www.yandex.ru/;cat index.html|grep -o -E 'yes" aria-label="[^"]+"' >text1;cat text1|sed -e 's/yes" aria-label=//g' >text2;cat text2|sed -e 's/"Котировки"/Погода/g' >text1;rm index.html;rm text2;
